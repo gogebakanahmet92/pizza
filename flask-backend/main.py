40c18725b35fa3f57a5c78dac52365abc8809b9b
ahmetgog <ahmet.gogebakan@vestel.com.tr>
2019-07-19 17:59:41 +0300

    React with Flask

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', token='HELLO WORLD!!!!')
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
