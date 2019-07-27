60cb4f27e0b4df01627e3fb7e3feaf31e89211ba
ahmetgog <ahmet.gogebakan@vestel.com.tr>
2019-07-27 22:43:50 +0300

    Image is done

--- flask-backend/create_task.py
+++ flask-backend/create_task.py
@@ -3,10 +3,13 @@ import numpy as np
 import pandas as pd
 import os
 import shutil
-from sklearn.metrics import mean_squared_error, r2_score
+
+from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn import linear_model
 from shutil import copyfile
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score
 
 
 
@@ -38,8 +41,6 @@ def get_pizza_type(file_name):
 def create_task_results(file_name,user):
 
     current_user = user
-    print('CURRENT USER')
-    print(current_user)
     data = pd.ExcelFile(file_name)
     sheet_name = data.sheet_names
     df = data.parse(sheet_name[0])
@@ -49,68 +50,85 @@ def create_task_results(file_name,user):
     sales_vector = df['SALES']
     pizza_vector = df['CATEGORY']
 
-    sales_data_dict = {}
     pizza_type = []
+
     for category in range(len(pizza_vector)):
-        if not pizza_vector[category] in sales_data_dict:
-            sales_data_dict[pizza_vector[category]] = [[],[]]
         if not pizza_vector[category] in pizza_type:
             pizza_type.append(pizza_vector[category])
 
-    for category in range(len(pizza_vector)):
-        tmpDt = datetime.datetime.strptime(str(dates_vector[category]), '%Y-%m-%d %H:%M:%S')
-        ms = tmpDt.timestamp()
-        sales_data_dict[pizza_vector[category]][0].append(ms)
-        sales_data_dict[pizza_vector[category]][1].append(sales_vector[category])
+    pizza_dict = {}
 
     for i in pizza_type:
-        i.encode('utf-8')
+        if not i in pizza_dict:
+            pizza_check =  pizza_vector == i
+            pizza_dict[i] = df[pizza_check]
 
 
-    X1 = sales_data_dict[pizza_vector[0]][0] 
-    X = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+    first_pizza = pizza_dict['Sicilian Pizza']
+    new_sales_vector = first_pizza['SALES']
+    new_dates_vector = first_pizza['DATE']
 
-    X= np.array(X)
-    X = X[:, np.newaxis] 
-
-    for i in range(len(pizza_type)):
-
-        Y = sales_data_dict[pizza_vector[i]][1]
-
-        
+    k = 0
+    for i in pizza_type:
+        pizza = pizza_dict[i]
+        new_sales_vector = pizza['SALES']
+        new_dates_vector = pizza['DATE']
 
 
 
-        poly_reg = PolynomialFeatures(degree=1)
+        print(new_sales_vector)
 
-        X_poly = poly_reg.fit_transform(X)
-        pol_reg = linear_model.LinearRegression()
+        index = [] 
+        a = 1
+        for j in new_dates_vector:
+            index.append(a)
+            a += 1
 
-        pol_reg.fit(X_poly, Y)
 
-        
-        plt.plot(X, Y, color='red')
-        plt.plot(X, pol_reg.predict(poly_reg.fit_transform(X)), color='blue')
-        plt.title('Truth or Bluff (Linear Regression)')
-        plt.xlabel('Position level')
-        plt.ylabel('Salary') 
+        X_eksen = np.array(index)
+        X_eksen = X_eksen[:, np.newaxis] 
+
+        Y_eksen = new_sales_vector
+
+
+
+        polynomial_features = PolynomialFeatures(degree=4,
+                                                include_bias=False)
+        linear_regression = LinearRegression()
+
+        pipeline = Pipeline([("polynomial_features", polynomial_features),
+                                ("linear_regression", linear_regression)])
+
+
+        pipeline.fit(X_eksen, Y_eksen)
+
+        scores = cross_val_score(pipeline, X_eksen, Y_eksen,
+                                scoring="neg_mean_squared_error", cv=10)
+
+        a = np.empty(35)
+        b = np.arange(1, 35, 1)
+        X_test = b
+        plt.figure()
+        plt.plot(X_test, pipeline.predict(X_test[:, np.newaxis]), label="Model")
+        plt.plot(X_eksen, Y_eksen, label="Samples") 
 
         current_user_file_name = '/images/' + current_user + '.png'
 
-        saving_file_name = current_user + pizza_type[i] + '.png'
-
+        saving_file_name = current_user + pizza_type[k] + '.png'
+        
 
         current_file = os.path.dirname(os.path.abspath(__file__)) + '/images/' + saving_file_name
         parent_directory = os.path.normpath(os.path.dirname(os.path.abspath(__file__)) + os.sep + os.pardir)
 
-        #print(current_file)
-        #plt.savefig(current_file)
-        
-        if i == 0:
+            #print(current_file)
+        plt.savefig(current_file)
+        #
+        # plt.show()
+            
+        if k == 0:
             copyfile(current_file, 'images/' + current_user + '.png')
+        k += 1
 
-
-        destination = parent_directory + '/react-frontend/src/pages/' + saving_file_name
 
         #print('Variance score: %.2f' % r2_score(Y, pol_reg.predict(poly_reg.fit_transform(X))))
 
